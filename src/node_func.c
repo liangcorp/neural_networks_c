9680f9245a92b1c4b3da46181af4ab214abdb07a
Chen Liang <chen.liang.cn@outlook.com>
2024-03-10 20:36:03 +0000

    initial commit

---
+++ src/node_func.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdlib.h>
+
+double *node_func(double **X, double *theta, int num_feat, int num_train)
+{
+	const int ZERO = 0;
+
+	double sum = 0.0L;
+	double *h_x = calloc(num_train, sizeof(double));
+
+	for (int i = ZERO; i < num_train; i++) {
+		sum = 0.0L;
+		for (int j = ZERO; j < num_feat; j++) {
+			sum += theta[j] * X[i][j];
+		}
+		h_x[i] = pow(M_E, sum) / (1 + pow(M_E, sum));
+	}
+
+	return h_x;
+}
